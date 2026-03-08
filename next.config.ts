bba20d527e5b68d7b1ca592cc1a0fcdba8e9e96d
ckouu <candra.kou@gmail.com>
2026-03-07 19:52:24 -0800

    Image path corrections

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: '/WAclinics',
   assetPrefix: '/WAclinics/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
