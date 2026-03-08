41886fbf5d781495c1a50f10468ede9a392313af
ckouu <candra.kou@gmail.com>
2026-03-07 17:44:01 -0800

    Updates

---
+++ static/chunks/app/layout-8ae1054ccedbb038.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},1455:e=>{e.exports={style:{fontFamily:"'Karla', 'Karla Fallback'",fontStyle:"normal"},className:"__className_4f468c"}},4827:(e,a,l)=>{Promise.resolve().then(l.t.bind(l,347,23)),Promise.resolve().then(l.t.bind(l,1455,23))}},e=>{var a=a=>e(e.s=a);e.O(0,[96,441,684,358],()=>a(4827)),_N_E=e.O()}]);
--- types/app/page.ts
+++ types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\candr\clinic-site\src\app\page.tsx
+// File: C:\Users\candr\WAclinics\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
